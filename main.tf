7dba91bf1db5f43426df87ddbe5784b2daa746fe
DeshnaBera <beradeshna@gmail.com>
2023-12-19 16:32:11 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mytf-state-file"
+    key    = "path/to/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0fa1ca9559f1892ec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-instance"
+  }
+}
