81ee4dd25708578d482abc3c5b9f96e12fda0325
DeshnaBera <beradeshna@gmail.com>
2023-12-20 15:14:04 +0530

    kgh

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ variable "environment" {
  type        = string
 }
 
+terraform{
+    backend "s3"{
+bucket         = "mytf-state-file-${var.environment}"
+key            = "path/to/terraform-${var.environment}.tfstate"
+region         = "us-east-1"
+encrypt        = true
+dynamodb_table = "TfStateLock-${var.environment}"
+    }
+}
+
 resource "aws_instance" "ec2" {
   ami = "ami-0fa1ca9559f1892ec"
   instance_type = "t2.micro"
