953d6eb922f244ff491696826761c6b36b864718
DeshnaBera <beradeshna@gmail.com>
2023-12-20 15:29:01 +0530

    kdr

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ variable "environment" {
 
 terraform{
     backend "s3"{
-bucket         = "mytf-state-file-${var.environment}"
-key            = "path/to/terraform-${var.environment}.tfstate"
+bucket         = "mytf-state-file"
+key            = "path/to/terraform.tfstate"
 region         = "us-east-1"
 encrypt        = true
-dynamodb_table = "TfStateLock-${var.environment}"
+dynamodb_table = "TfStateLock"
     }
 }
 
